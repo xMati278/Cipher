23c090febcbd38628a2f379722d6713f72dc4857
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-07 17:46:53 +0200

    Update buffer_dict

--- src/buffer_dict.py
+++ src/buffer_dict.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class BufferDict:
-    text_before: str
-    text_after: str
-    rot_type: str
-    status: str
+    text_before: str = ""
+    text_after: str = ""
+    rot_type: str = ""
+    status: str = ""
 
     def to_dict(self):
         return {
@@ -15,3 +15,21 @@ class BufferDict:
             'rot_type': self.rot_type,
             'status': self.status
         }
+
+    @classmethod
+    def to_object_list(cls, dict_list) -> list:
+        object_list = []
+        for item in dict_list:
+            try:
+                obj = cls(**item)
+                object_list.append(obj)
+            except TypeError as e:
+                raise TypeError("Invalid input data for BufferDict") from e
+        return object_list
+
+    @classmethod
+    def to_dict_list(cls, object_list) -> list:
+        dict_list = []
+        for obj in object_list:
+            dict_list.append(obj.to_dict())
+        return dict_list
