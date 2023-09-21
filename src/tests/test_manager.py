254b9e6d75ad986a60fdff64a9ae0fe35171bed6
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-21 16:23:05 +0200

    test(manager): update tests

---
+++ src/tests/test_manager.py
@@ -0,0 +1,55 @@
+import pytest
+from src.menu import Menu
+from src.file_handler import FileHandler
+from src.manager import Manager
+from src.text import Text
+from unittest import mock
+
+
+def test_manager_initialization():
+    manager = Manager()
+    assert isinstance(manager.file_handler, FileHandler)
+    assert isinstance(manager.buffer, list)
+
+
+def test_manager_start_encryption():
+    mock_args = ["1", "rot_13", "hello", "test", "no", "0"]
+    with mock.patch("builtins.input", side_effect=mock_args):
+        manager = Manager()
+        manager.start()
+    assert len(manager.buffer) == 1
+    assert manager.buffer[0].status == "encrypted"
+
+
+def test_manager_start_decryption():
+    mock_args = ["2", "rot_13", "uryyb", "test", "no", "0"]
+    with mock.patch("builtins.input", side_effect=mock_args):
+        manager = Manager()
+        manager.start()
+    assert len(manager.buffer) == 1
+    assert manager.buffer[0].status == "decrypted"
+
+
+def test_manager_start_read_file():
+    mock_args = ["1", "rot_13", "hello", "test", "yes", "0"]
+    with mock.patch("builtins.input", side_effect=mock_args):
+        manager = Manager()
+        manager.file_handler.read = lambda filename: [
+            {"text": "hello", "rot_type": "Rot13", "status": "encrypted"}
+        ]
+        manager.start()
+    assert len(manager.buffer) == 2
+    assert isinstance(manager.buffer[1], Text)
+    assert manager.buffer[0]["status"] == "encrypted"
+
+
+def test_manager_start_write_file():
+    mock_args = ["1", "rot_13", "hello", "test", "no", "0"]
+    with mock.patch("builtins.input", side_effect=mock_args):
+        manager = Manager()
+        manager.file_handler.read = lambda filename: []
+        manager.file_handler.write = lambda filename, data, read: None
+        manager.start()
+    assert len(manager.buffer) == 1
+    assert isinstance(manager.buffer[0], Text)
+    assert manager.buffer[0].status == "encrypted"
