2e633412a649464147d36ebddc1b787f35a71032
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-21 13:23:50 +0200

    chore(file_handler): changed path to save json files

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,61 +1,93 @@
 import json
 import os
-from src.buffer_dict import BufferDict
+from src.buffer import Buffer
 
 
 class FileHandler:
-
-    def write(self, filename: str, data: list) -> list:
-        """
-        The method writes data from the "data" parameter to a file named "filename"
-
-        :param filename: "example" or "example.JSON" - filename or filename with extension .JSON
-        :param data: parameter for saving data
-        """
-
-        if data is None:
-            raise ValueError
-
-        try:
-            if not filename.endswith(".json"):
-                filename += ".json"
-                data = BufferDict.to_dict_list(data)
-                print(data)
-
-            with open(filename, "w") as file:
-                json.dump(data, file, indent=4)
-
-            return data
-
-        except Exception as e:
-            print(e)
+    def __init__(self):
+        self.buffer_operation = Buffer()
+        self.data_folder = "..//json_files"
 
     @staticmethod
-    def read(filename: str) -> list:
+    def validate_file_extension(filename: str) -> str:
+        """
+        Validates the filename format.
+
+        :param filename: filename to validate
+
+        :return: filename in the correct format
+        """
+
+        if not filename.endswith(".json"):
+            filename += ".json"
+
+        return filename
+
+    def write_to_file(self, filename: str, data: list) -> None:
+        """
+        Writes data to a JSON file.
+
+        :param filename: file name in JSON format
+        :param data: data to be written to a file
+        """
+
+        file_path = os.path.join(self.data_folder, filename)
+
+        with open(file_path, "w") as file:
+            json.dump(data, file, indent=4)
+
+    def write(self, filename: str, data: list, read: bool):
+        """
+        Checks the correctness of the data to be saved and calls the save function.
+
+        :param filename: file name in JSON format
+        :param data: data to be written to a file
+        :param read: information whether data was downloaded from the file
+        """
+
+        try:
+            if data is None:
+                raise ValueError("Data should not be None.")
+
+            filename = self.validate_file_extension(filename=filename)
+
+            if not read:
+                data_to_save = self.read(filename=filename)
+                data_to_save = self.buffer_operation.to_dict_list(obj_list=data_to_save)
+                data_to_save += data
+                self.write_to_file(filename, data_to_save)
+            else:
+                self.write_to_file(filename, data)
+
+        except (FileNotFoundError, ValueError) as e:
+            print(f"src.file_handler.write Error: {e}")
+
+    def read(self, filename: str) -> list:
         """
         The method reads data from the file "filename" and returns it
 
         :param filename: "example" or "example.JSON" - filename or filename with extension .JSON
+
         :return: a list of dictionaries based on the given file
         """
 
         try:
-            if not filename.endswith(".json"):
-                filename += ".json"
+            filename = self.validate_file_extension(filename=filename)
+            file_path = os.path.join(self.data_folder, filename)
 
-            if not os.path.exists(filename):
-                with open(filename, "w") as file:
-                    json.dump([], file)
-                return []
+            if not os.path.exists(file_path):
+                raise FileNotFoundError(f"File: {filename} not found.")
 
-            with open(filename, "r") as file:
+            with open(file_path, "r") as file:
                 try:
                     cipher_list = json.load(file)
-                    cipher_list = BufferDict.to_object_list(cipher_list)
 
                 except json.JSONDecodeError:
                     cipher_list = []
+                    return cipher_list
 
-                return cipher_list
-        except Exception as e:
-            print(e)
+                valid_list = self.buffer_operation.to_object_list(dict_list=cipher_list)
+                return valid_list
+
+        except FileNotFoundError as e:
+            print(f"src.file_handler.read Error: {e}")
