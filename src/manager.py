82366c333627b8957200c64969dfdfcab1c8adfa
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-26 17:10:49 +0200

    chore(manager): adding options for saving and loading data

--- src/manager.py
+++ src/manager.py
@@ -24,42 +24,47 @@ class Manager:
             if choice["mode"] in [1, 2]:
                 self.process_choice(choice)
 
+            if choice["mode"] == 3:
+                Buffer.data = self.load_json_file()
+
+            if choice["mode"] == 4: # TODO Save
+                self.save_to_json_file()
+
             print(f"Current buffer: {self.buffer}")
 
     def process_choice(self, choice):
-        mode, shift, message, filename, read_file = choice.values()
+        mode, shift, message = choice.values()
 
         rot_version = Rot.get_rot(shift)
 
-        if read_file:
-            self.buffer = self.file_handler.read(filename=filename)
-
         if mode == 1:
-            self.encrypt_message(rot_version, message, filename, read_file)
+            self.encrypt_message(rot_version, message)
         else:
-            self.decrypt_message(rot_version, message, filename, read_file)
+            self.decrypt_message(rot_version, message)
 
-    def encrypt_message(self, rot_version, message, filename, read_file):
+    def encrypt_message(self, rot_version, message):
         status = "encrypted"
         encrypted_msg = rot_version.encrypt(msg=message)
-        self.update_buffer_and_write(
-            encrypted_msg, rot_version, status, filename, read_file
-        )
+        new_text_object = self.create_text_object(message=encrypted_msg, status=status, rot_version=rot_version)
+        Buffer.data.append(new_text_object)
 
-    def decrypt_message(self, rot_version, message, filename, read_file):
+    def decrypt_message(self, rot_version, message):
         status = "decrypted"
         decrypted_msg = rot_version.decrypt(msg=message)
-        self.update_buffer_and_write(
-            decrypted_msg, rot_version, status, filename, read_file
-        )
+        new_text_object = self.create_text_object(message=decrypted_msg, status=status, rot_version=rot_version)
+        Buffer.data.append(new_text_object)
 
-    def update_buffer_and_write(
-        self, message, rot_version, status, filename, read_file
-    ):
+    def load_json_file(self) -> list:
+        filename = Menu().get_filename()
+        data_to_load = self.file_handler.read(filename=filename)
+        return data_to_load
+
+    def save_to_json_file(self):
+        filename = Menu().get_filename()
+        self.file_handler.write(filename=filename, data=Buffer.data)
+
+    def create_text_object(self, message, rot_version, status):
         text = Text(
             text=message, rot_type=rot_version.__class__.__name__, status=status
         )
-
-        self.buffer.append(text)
-        data_to_write = Buffer().to_dict_list(self.buffer)
-        self.file_handler.write(filename=filename, data=data_to_write, read=read_file)
+        return text
