c2973558e15d9d205e4f5a803e2f7d37bc5b9eb6
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-26 13:25:50 +0200

    feat(logging_config): added custom logger config

---
+++ logs/logging_config.py
@@ -0,0 +1,14 @@
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+log_filename = "app.log"
+file_handler = logging.FileHandler(log_filename)
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(file_handler)
