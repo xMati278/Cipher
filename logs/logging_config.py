97243351fd22b96dab9f72b54137dcfd3ecf0f2b
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-26 17:30:54 +0200

    chore(rot): changed typing in methods

--- logs/logging_config.py
+++ logs/logging_config.py
@@ -3,11 +3,11 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-log_filename = "app.log"
+log_filename = "..//logs//app.log"
 file_handler = logging.FileHandler(log_filename)
 file_handler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 
 logger = logging.getLogger()
