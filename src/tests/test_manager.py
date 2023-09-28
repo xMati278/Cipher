cd63febf2434fffc37411e9ed3395a824a94fcca
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-28 12:28:43 +0200

    test(manager): update tests

--- src/tests/test_manager.py
+++ src/tests/test_manager.py
@@ -4,30 +4,23 @@ from unittest import mock
 from src.buffer import Buffer
 
 
+class TestManager:
 
-def test_manager_initialization():
-    manager = Manager()
-    assert isinstance(manager.file_handler, FileHandler)
-    assert isinstance(manager.buffer, list)
+    def test_manager_start_encryption(self):
+        mock_args = ["1", "rot_13", "hello", "0"]
+        with mock.patch("builtins.input", side_effect=mock_args):
+            manager = Manager()
+            manager.start()
+        assert len(Buffer.data) == 1
+        assert Buffer.data[0].status == "encrypted"
 
-
-def test_manager_start_encryption():
-    mock_args = ["1", "rot_13", "hello", "test", "no", "0"]
-    with mock.patch("builtins.input", side_effect=mock_args):
-        manager = Manager()
-        manager.start()
-    assert len(manager.buffer) == 1
-    assert manager.buffer[0].status == "encrypted"
-
-
-def test_manager_start_decryption():
-    mock_args = ["2", "rot_13", "uryyb", "test", "no", "0"]
-    with mock.patch("builtins.input", side_effect=mock_args):
-        manager = Manager()
-        manager.start()
-    assert len(manager.buffer) == 1
-    assert manager.buffer[0].status == "decrypted"
-
+    def test_manager_start_decryption(self):
+        mock_args = ["2", "rot_13", "uryyb", "0"]
+        with mock.patch("builtins.input", side_effect=mock_args):
+            manager = Manager()
+            manager.start()
+        assert len(Buffer.data) == 2
+        assert Buffer.data[1].status == "decrypted"
 
     def test_manager_start_read_file(self):
         mock_args = ["1", "rot_13", "hello", "0"]
