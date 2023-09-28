0d309475a54156ef149d745370e05859ac45e29c
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-09-28 12:27:16 +0200

    test(manager): update tests

--- src/tests/test_manager.py
+++ src/tests/test_manager.py
@@ -1,7 +1,8 @@
-from src.file_handler import FileHandler
 from src.manager import Manager
 from src.text import Text
 from unittest import mock
+from src.buffer import Buffer
+
 
 
 def test_manager_initialization():
@@ -29,25 +30,24 @@ def test_manager_start_decryption():
 
 
     def test_manager_start_read_file(self):
-        mock_args = ["1", "rot_13", "hello", "test", "yes", "0"]
+        mock_args = ["1", "rot_13", "hello", "0"]
         with mock.patch("builtins.input", side_effect=mock_args):
             manager = Manager()
             manager.file_handler.read_to_buffer = lambda filename: [
                 {"text": "hello", "rot_type": "Rot13", "status": "encrypted"}
             ]
             manager.start()
-        assert len(manager.buffer) == 2
-        assert isinstance(manager.buffer[1], Text)
-        assert manager.buffer[0]["status"] == "encrypted"
-
+        assert len(Buffer.data) == 3
+        assert isinstance(Buffer.data[1], Text)
+        assert Buffer.data[2].status == "encrypted"
 
     def test_manager_start_write_file(self):
-        mock_args = ["1", "rot_13", "hello", "test", "no", "0"]
+        mock_args = ["1", "rot_13", "hello", "0"]
         with mock.patch("builtins.input", side_effect=mock_args):
             manager = Manager()
             manager.file_handler.read_to_buffer = lambda filename: []
             manager.file_handler.write = lambda filename, data, read: None
             manager.start()
-        assert len(manager.buffer) == 1
-        assert isinstance(manager.buffer[0], Text)
-        assert manager.buffer[0].status == "encrypted"
+        assert len(Buffer.data) == 4
+        assert isinstance(Buffer.data[0], Text)
+        assert Buffer.data[3].status == "encrypted"
