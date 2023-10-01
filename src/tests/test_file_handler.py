25040bbbc1fce8dbaccca2f6b95f7c245d29f67f
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-10-01 15:29:34 +0200

    chore(test_buffer): removing unnecessary part of the code

--- src/tests/test_file_handler.py
+++ src/tests/test_file_handler.py
@@ -122,3 +122,29 @@ def test_write(data_to_write, read, expected_data):
             read_data = json.load(file)
 
         assert read_data == expected_data
+
+def test_read():
+    data = [
+    {
+        "text": "grfg",
+        "rot_type": "Rot13",
+        "status": "encrypted"
+    },
+    {
+        "text": "test",
+        "rot_type": "Rot13",
+        "status": "decrypted"
+    }
+    ]
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_file = os.path.join(temp_dir, "temp.json")
+        os.makedirs(os.path.dirname(temp_file), exist_ok=True)
+        FileHandler().write_to_file(filename=temp_file, data=data)
+        loaded_data = FileHandler().read_to_buffer(filename=temp_file)
+
+    assert isinstance(loaded_data[0], Text)
+    assert isinstance(loaded_data[1], Text)
+    assert data[0]["text"] == loaded_data[0].text
+    assert data[1]["rot_type"] == loaded_data[1].rot_type
+    assert data[0]["status"] == loaded_data[0].status
